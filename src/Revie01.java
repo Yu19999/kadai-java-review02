92251810c3c4bff6365f0dcfc81d2830462692cd
yu <yu912912@gmail.com>
2024-05-10 21:55:29 +0900

    更新

--- src/Revie01.java
+++ src/Revie01.java
@@ -3,14 +3,14 @@ public class Revie01 {
 
     public static void main(String[] args) {
         int price = 1500;
-        double taxrate = 0.1
-        int result = price + tax(price,taxrate)
+        double taxrate = 0.1;
+        int result = price + tax(price,taxrate);
         System.out.println(price + "円の商品の税込価格は" + result + "円（消費税は" + tax(price,taxrate) + "円）です。");
 
     }
 
     public static int tax(int price, double taxrate) {
         int result = (int) (price * taxrate);
-        result return
+        return result;
     }
 }
