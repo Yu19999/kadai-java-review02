28e419e2e2da9d57afa0ed2d8cc806aa1a473a1f
yu <yu912912@gmail.com>
2024-05-14 22:56:21 +0900

    3回目

--- src/Review02.java
+++ src/Review02.java
@@ -4,11 +4,11 @@ public class Review02 {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("Fizz");
+                System.out.println("Fizz Buzz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else if (i % 3 == 0) {
-                System.out.println("Fizz Buzz");
+                System.out.println("Fizz");
             } else {
                 System.out.println(i);
             }
